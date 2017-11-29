d9a8e2a286efaa762f210d9e6498a6b992368ab1
zhangmeng <mattwho@163.com>
2017-11-29 09:55:38 +0800

    add index banner

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -3,9 +3,22 @@
 require('./index.scss')
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-var navSide = require('page/common/nav-side/index.js');
+require('util/slider/index.js');
+var _mm = require('util/mm.js');
+var templateBanner = require('./index.matt');
 require('node_modules/_font-awesome@4.7.0@font-awesome/css/font-awesome.min.css');
 
-navSide.init({
-    name: 'user-center'
-})
+
+$(function() {
+    var bannerHtml = _mm.renderHtml(templateBanner)
+    $('.banner-con').html(bannerHtml);
+    var $slider = $('.banner').unslider({
+        dots: true,
+    });
+    // 前一张和后一张的时间绑定
+    $('.banner-con .banner-arrow').click(function() {
+        var forward = $(this).hasClass('prew') ? 'prev' : 'next';
+        console.log(forward)
+        $slider.data('unslider')[forward]();
+    })
+});
